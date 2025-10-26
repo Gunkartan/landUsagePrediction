9fad8df52bfbafb34afb0b9f207fa7f10af000e0
Karun Tancharoen <harukajidoto@gmail.com>
2025-10-26 22:34:42 +0700

    Added class sampling.

--- code/sample_classes.py
+++ code/sample_classes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def stratified_sample(group: pd.DataFrame):
-    if group.Name == 13:
+    if group.name == 13:
         n = min(len(group), n_samples_other)
 
     else:
@@ -10,8 +10,8 @@ def stratified_sample(group: pd.DataFrame):
     return group.sample(n, random_state=42)
 
 if __name__ == '__main__':
-    df = pd.read_csv('../csvs/rawCrop.csv')
+    df = pd.read_csv('../csvs/rawCropSampled.csv')
     n_samples_crop = 3000
     n_samples_other = 10000
     df_sampled = df.groupby('Crops', group_keys=False).apply(stratified_sample)
-    df_sampled.to_csv('../csvs/rawCrop.csv', index=False)
+    df_sampled.to_csv('../csvs/rawCropSampled.csv', index=False)
