b75cf8b8dcee556af6cc02c893f54259abd1ffcb
Karun Tancharoen <harukajidoto@gmail.com>
2025-11-28 08:55:44 +0700

    feat(dataset): add more samples to all classes

--- code/sample_classes.py
+++ code/sample_classes.py
@@ -11,7 +11,7 @@ def stratified_sample(group: pd.DataFrame):
 
 if __name__ == '__main__':
     df = pd.read_csv('../csvs/rawCrop.csv')
-    n_samples_crop = 96000
-    n_samples_other = 192000
+    n_samples_crop = 192000
+    n_samples_other = 384000
     df_sampled = df.groupby('Crops', group_keys=False).apply(stratified_sample)
     df_sampled.to_csv('../csvs/rawCropSampled.csv', index=False)
